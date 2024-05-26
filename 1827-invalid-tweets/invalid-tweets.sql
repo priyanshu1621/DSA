f065c6485762bd507907528d10355aab61da5f24
Priyanshu Gaur <82864666+priyanshu1621@users.noreply.github.com>
2024-05-26 15:49:32 +0530

    Time: 1057 ms (73.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+-- select tweet_id from Tweets where OCTET_LENGTH(content) > '15'; 
+select tweet_id from Tweets where CHARACTER_LENGTH(content) > '15'; 
+-- select tweet_id from Tweets where CHAR_LENGTH(content) > '15'; 
