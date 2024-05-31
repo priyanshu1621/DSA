3f7e69643fe784945dd5c089a545dcbd1aa6c522
Priyanshu Gaur <82864666+priyanshu1621@users.noreply.github.com>
2024-06-01 02:03:41 +0530

    Time: 14 ms (8.86%) | Memory: 14.4 MB (9.91%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -2,26 +2,46 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
 
-        // Optimal approach
-
-        long long xorr = 0;
-
-        for(int &number : nums){
-            xorr ^= number;
-        }
-
-        int mask = (xorr) & (-xorr);
-        int groupa = 0 , groupb = 0;
+        unordered_map<int, int> mp;
 
         for(int &num : nums){
-            if(mask & num)
-                groupa ^= num;
-            else{
-                groupb ^= num;
-            }
+            mp[num]++;
         }
 
-        return {groupa ,groupb};
+        vector<int> ans;
+
+        for(const auto& num : mp){
+
+            if( num.second == 1) ans.push_back(num.first);         
+        }
+
+        return ans;
         
     }
+
+// Bit manipulation TC: O(n)
+    // vector<int> singleNumber(vector<int>& nums) {
+
+    //     // Optimal approach
+
+    //     long long xorr = 0;
+
+    //     for(int &number : nums){
+    //         xorr ^= number;
+    //     }
+
+    //     int mask = (xorr) & (-xorr);
+    //     int groupa = 0 , groupb = 0;
+
+    //     for(int &num : nums){
+    //         if(mask & num)
+    //             groupa ^= num;
+    //         else{
+    //             groupb ^= num;
+    //         }
+    //     }
+
+    //     return {groupa ,groupb};
+        
+    // }
 };
