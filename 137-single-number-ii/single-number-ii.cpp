387c92d06176beaa55e656394169e4788a925e88
Priyanshu Gaur <82864666+priyanshu1621@users.noreply.github.com>
2024-05-31 23:59:46 +0530

    Time: 4 ms (79.68%) | Memory: 12 MB (29.95%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
+    // int singleNumber(vector<int>& nums) {
+    //     unordered_map<int, int> m;
+
+    //     for(auto i : nums){
+    //         m[i]++;
+    //     }
+
+    //     for(auto x : m){
+    //         if(x.second == 1){
+    //             return x.first;
+    //         }
+    //     }
+
+
+    //     //     for( auto pair : m) {
+    //     //     std::cout << "Element " << pair.first << " appears " << pair.second << " times." << std::endl;
+    //     // }
+
+    //     return -1;
+    // }
+
+    // 2Nd approach
+
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> m;
+        
 
-        for(auto i : nums){
-            m[i]++;
+        sort(nums.begin() , nums.end());
+
+        for(int i = 1 ; i < nums.size(); i+=3){
+
+            if( nums[i] != nums[i-1] ) return nums[i-1];
+
         }
-
-        for(auto x : m){
-            if(x.second == 1){
-                return x.first;
-            }
-        }
-
-
-        //     for( auto pair : m) {
-        //     std::cout << "Element " << pair.first << " appears " << pair.second << " times." << std::endl;
-        // }
-
-        return -1;
+    
+        return nums[nums.size() -1 ];
     }
 };
