ee34a9f2135d8b8a1fb3c3f0a9ee0e21b8d682b7
Priyanshu Gaur <82864666+priyanshu1621@users.noreply.github.com>
2024-06-09 00:58:44 +0530

    Time: 188 ms (35.89%) | Memory: 134.8 MB (31.96%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,34 +1,57 @@
 class Solution {
 public:
    bool checkSubarraySum(vector<int>& nums, int k) {
-    // Map to store the remainder when cumulative sum is divided by k
-    unordered_map<int, int> remainder_map;
-    // Initialize with remainder 0 at index -1 to handle edge case of subarrays starting at the beginning
-    remainder_map[0] = -1;
-    int sum = 0;
-    
-    for (int i = 0; i < nums.size(); i++) {
-        sum += nums[i];
-        int remainder = sum % k;
-        
-        // Handling negative remainders
-        if (remainder < 0) {
-            remainder += k;
-        }
-        
-        // Check if this remainder has been seen before
-        if (remainder_map.find(remainder) != remainder_map.end()) {
-            // Check if the subarray length is at least 2
-            if (i - remainder_map[remainder] > 1) {
-                return true;
+   
+        unordered_map<int , int> reminder_map;
+        int sum;
+        reminder_map[0] = -1;
+
+        for(int i = 0; i< nums.size(); i++){
+
+            sum += nums[i]; 
+            
+            int remainder = sum % k ;
+
+            if(reminder_map.find(remainder) != reminder_map.end()){
+                if(i - reminder_map[remainder] >= 2){
+                    return true;
+                }
+            }else{
+                reminder_map[remainder] = i ;
             }
-        } else {
-            // Store the first occurrence of this remainder
-            remainder_map[remainder] = i;
         }
-    }
-    
-    // No valid subarray found
-    return false;
-}
+
+        return false;
+
+
+
+
+    // // Map to store the remainder when cumulative sum is divided by k
+    // unordered_map<int, int> remainder_map;
+    // // Initialize with remainder 0 at index -1 to handle edge case of subarrays starting at the beginning
+    // remainder_map[0] = -1;
+    // int sum = 0;
+    // for (int i = 0; i < nums.size(); i++) {
+    //     sum += nums[i];
+    //     int remainder = sum % k;   
+    //     // Handling negative remainders
+    //     if (remainder < 0) {
+    //         remainder += k;
+    //     }     
+    //     // Check if this remainder has been seen before
+    //     if (remainder_map.find(remainder) != remainder_map.end()) {
+    //         // Check if the subarray length is at least 2
+    //         if (i - remainder_map[remainder] > 1) {
+    //             return true;
+    //         }
+    //     } else {
+    //         // Store the first occurrence of this remainder
+    //         remainder_map[remainder] = i;
+    //     }
+    // }
+    // // No valid subarray found
+    // return false;
+    // }
+
+   }
 };
