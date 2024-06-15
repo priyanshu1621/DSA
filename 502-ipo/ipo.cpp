0c532720a2734b2ee54145ff4f93ccc2660bb2b5
Priyanshu Gaur <82864666+priyanshu1621@users.noreply.github.com>
2024-06-15 23:47:23 +0530

    Time: 123 ms (94.27%) | Memory: 81.6 MB (83.55%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -8,7 +8,7 @@ public:
         }
 
         // Sort the project in te basis of capital default it sort on basis of first
-        sort(projects.begin(), projects.end());
+        sort(projects.begin(), projects.end());     // ->   O(Nlog(n)) -> N = size of size
 
         priority_queue<int> maxHeap;
         int i = 0;
@@ -18,7 +18,7 @@ public:
             // Checking the capital values with w and also that the i should not go out of bound
         
             while(i < profits.size() && projects[i].first <= w  ){
-                maxHeap.push(projects[i].second);
+                maxHeap.push(projects[i].second);         // heap push || pop -> O(Nlog(n)) , here N = K , and log(n) = is heap operation complexity
                 i++;
             }
 
@@ -32,7 +32,11 @@ public:
             w += maxHeap.top();
             maxHeap.pop();
 
-        }
+        }  
+
+        // Total TC:->  O(Nlog(n))  + O(Nlog(n))  =  O(Nlog(n))
+        // SC  -> vector<> -> o(n)  +  Heap -> O(n)  =    O(n)
+
 
         return w;
     }
