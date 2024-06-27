cd7fc53fea6d46ef9a074eb399ffc5e8da871b49
Priyanshu Gaur <82864666+priyanshu1621@users.noreply.github.com>
2024-06-27 14:56:42 +0530

    Time: 141 ms (48.65%) | Memory: 70.7 MB (92.91%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -6,29 +6,41 @@ public:
         // By using the count of degree we can find the graph is star or not
         // if degree equal to (n-1) nodes means equal to edges.size()
 
-        unordered_map<int, int>degree;
+        // unordered_map<int, int>degree;
 
-        for(auto &i : edges ){
-            int u = i[0];
-            int v = i[1];
+        // for(auto &i : edges ){
+        //     int u = i[0];
+        //     int v = i[1];
 
-            degree[u]++;
-            degree[v]++;
+        //     degree[u]++;
+        //     degree[v]++;
+        // }
+
+        // for(auto j : degree){
+        //     if(j.second == edges.size() ){
+        //         return j.first;
+        //     }
+
+        // }
+
+
+        // //  if not find the star
+        // return -1;
+
+
+     // Approach 2 : observation is to compare the 2 edges and see we fount the common
+
+        vector<int> first = edges[0];   // {p, q}
+        vector<int> second = edges[1]; //  {q, g}
+
+        if(first[0] == second[0] || first[0] == second[1]){
+            return first[0];
         }
 
-        for(auto j : degree){
-            if(j.second == edges.size() ){
-                return j.first;
-            }
+        // If the ove if not found the eged then it should be the edge 1 ,
+        //  as in the question it is said that star is present
 
-        }
-
-
-        //  if not find the star
-        return -1;
-
-
-
+        return first[1];
        
     }
 };
