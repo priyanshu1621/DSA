0c916f9fdec222160ab0ee29b72f05c56b8820d5
Priyanshu Gaur <82864666+priyanshu1621@users.noreply.github.com>
2025-06-26 19:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (51.37%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,52 +1,41 @@
 class Solution {
 public:
 
-    static bool cmp(pair<int,int>a, pair<int, int>b){
+    static bool cmp(pair<int , int> a, pair<int , int>b){
         return a.first < b.first;
     }
 
 
-
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>>data; // pair of((value, original index))
+
+        vector<pair<int, int>>data;
         vector<int>ans;
 
-        for(int i = 0; i< nums.size(); i++){
-            data.push_back({nums[i],i});
+        for(int i = 0; i < nums.size(); i++){
+            data.push_back({nums[i], i});
         }
 
-        // sort 
-        sort(data.begin(), data.end(),cmp); //sort based on values
+        sort(data.begin(), data.end(), cmp);
 
         int start = 0;
-        int end = data.size() - 1;
+        int end = nums.size() - 1;
 
         while(start < end){
-            int sum = data[start].first + data[end].first;
+            int sum = (data[start].first + data[end].first);
+
             if(sum == target){
                 ans.push_back(data[start].second);
                 ans.push_back(data[end].second);
                 break;
             }
-            else if(sum > target){
-                end--;
-            }
+            else if(sum > target) end--;
             else{
                 start++;
             }
         }
-        return ans;
-    
 
-        // vector<int>ans;
-        // for(int i =0; i<nums.size()-1; i++){
-        //     for(int j = i+1; j<nums.size(); j++){
-        //         if(nums[i] + nums[j] == target)
-        //             {ans.push_back(i);
-        //             ans.push_back(j);
-        //             return ans;}
-        //     }
-        // }
-        // return {};
+        return ans;
+
+        
     }
 };
